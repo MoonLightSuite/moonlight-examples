cb2f1f859688bc9b6ee14c883fbb50b34449ccdb
Simone <simone.silvetti@gmail.com>
2019-01-28 10:18:47 +0100

    added signal creator for Taliro

--- eu.quanticol.moonlight.examples.example1/src/main/java/eu/quanticol/moonlight/matlab/MatlabProva.java
+++ eu.quanticol.moonlight.examples.example1/src/main/java/eu/quanticol/moonlight/matlab/MatlabProva.java
@@ -1,14 +1,29 @@
 package eu.quanticol.moonlight.matlab;
 
 import eu.quanticol.moonlight.configurator.Matlab;
+import eu.quanticol.moonlight.formula.Formula;
+import eu.quanticol.moonlight.io.FormulaToTaliro;
+import eu.quanticol.moonlight.signal.SignalCreator;
+import eu.quanticol.moonlight.signal.SignalCreatorDouble;
+import eu.quanticol.moonlight.signal.VariableArraySignal;
+import eu.quanticol.moonlight.util.FormulaGenerator;
+import eu.quanticol.moonlight.util.FutureFormulaGenerator;
 import org.n52.matlab.control.MatlabInvocationException;
 import org.n52.matlab.control.MatlabProxy;
+import org.n52.matlab.control.extensions.MatlabNumericArray;
+import org.n52.matlab.control.extensions.MatlabTypeConverter;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.function.Function;
 
 public class MatlabProva {
+    private static FormulaToTaliro toTaliro = new FormulaToTaliro();
+
 
 
     public static void main2(String[] args) throws MatlabInvocationException {
@@ -19,7 +34,7 @@ public class MatlabProva {
         System.out.println("done");
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main3(String[] args) throws Exception {
         ClassLoader classLoader = ClassLoader.getSystemClassLoader();
         MatlabProxy eng = Matlab.startMatlab();
         InputStream resourceAsStream = classLoader.getResourceAsStream("prova.m");
@@ -62,6 +77,40 @@ public class MatlabProva {
         }
         after = System.currentTimeMillis();
         System.out.println("Breach Avg. Time (msec) ("+ nReps +" repetitions): " +(after-before)/1000.);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Map<String, Function<Double, Double>> functionalMap = new HashMap<>();
+        functionalMap.put("a", t -> Math.pow(t, 2.));
+        functionalMap.put("b", Math::cos);
+        functionalMap.put("c", Math::sin);
+        SignalCreatorDouble signalCreator = new SignalCreatorDouble(functionalMap);
+        double[] time = signalCreator.generateTime(0, 100, 0.1);
+        double[][] values = signalCreator.generateValues(time);
+        VariableArraySignal signal = signalCreator.generate(0, 100, 0.1);
+        FormulaGenerator formulaGenerator = new FutureFormulaGenerator(new Random(1), signal.getEnd()/3, signalCreator.getVariableNames());
+        Formula generatedFormula = formulaGenerator.getFormula(4);
+        System.out.println(generatedFormula.toString());
+        System.out.println(toTaliro.toTaliro(generatedFormula));
+        System.out.println(toTaliro.createPrefix(signalCreator));
+        MatlabProxy eng = Matlab.startMatlab();
+        eng.setVariable("T",time);
+        MatlabTypeConverter processor = new MatlabTypeConverter(eng);
+        processor.setNumericArray("M", new MatlabNumericArray(values, null));
+        eng.eval("M = transpose(M);");
+        eng.eval("T = transpose(T);");
+        eng.eval(toTaliro.toTaliro(generatedFormula));
+        eng.eval(toTaliro.createPrefix(signalCreator));
+        eng.eval("taliroRes = taliro(M,T);");
+        double[] Z = (double[] )eng.getVariable("taliroRes");
+        System.out.println("Taliro Robustness: " +Z[0]);
+        long before = System.currentTimeMillis();
+        int nReps = 30;
+        for (int i = 0; i < nReps; i++) {
+            eng.eval("taliroRes = taliro(M,T);");
+        }
+        long after = System.currentTimeMillis();
+        System.out.println("Taliro Avg. Time (msec) ("+ nReps +" repetitions): " +(after-before)/1000.);
     }
 
 
