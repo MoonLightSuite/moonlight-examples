0d54be30c052af22f1451369debe1008577a7fe5
lauranenzi <laura.nenzi@gmail.com>
2019-03-12 16:44:59 +0100

    cityExample

--- eu.quanticol.moonlight.examples.example1/src/main/java/eu/quanticol/moonlight/examples/example1/mainSp.java
+++ eu.quanticol.moonlight.examples.example1/src/main/java/eu/quanticol/moonlight/examples/example1/mainSp.java
@@ -8,59 +8,66 @@ import eu.quanticol.moonlight.signal.GraphModel;
 import eu.quanticol.moonlight.signal.SpatialModel;
 import eu.quanticol.moonlight.signal.SpatialSignal;
 import sun.font.TrueTypeFont;
+import java.util.*;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.function.BiFunction;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 
 public class mainSp {
     public static void main(String[] argv) {
-        Integer size = 4;
+        Integer size = 7;
         GraphModel<Double> city = new GraphModel<Double>(size);
-        city.add(0, 3.0, 1);
-        city.add(0, 7.0, 3);
-        city.add(2, 5.0, 1);
-        city.add(2, 4.0, 3);
-        city.add(3, 7.0, 0);
-        city.add(1, 5.0, 2);
-        city.add(3, 4.0, 2);
+        city.add(0, 2.0, 1);
+        city.add(1, 2.0, 0);
+        city.add(0, 2.0, 5);
+        city.add(5, 2.0, 0);
+        city.add(1, 9.0, 2);
+        city.add(2, 9.0, 1);
+        city.add(2, 3.0, 3);
+        city.add(3, 3.0, 2);
+        city.add(3, 6.0, 4);
+        city.add(4, 6.0, 3);
+        city.add(4, 7.0, 5);
+        city.add(5, 7.0, 4);
+        city.add(6, 4.0, 1);
+        city.add(1, 4.0, 6);
+        city.add(6, 15.0, 3);
+        city.add(3, 15.0, 6);
 
-        SpatialSignal<Boolean> stop = new SpatialSignal<>(size);
-        ArrayList<Boolean> Stop=new ArrayList<Boolean>();
-        Stop.add(Boolean.TRUE);
-        Stop.add(Boolean.FALSE);
-        Stop.add(Boolean.FALSE);
-        Stop.add(Boolean.TRUE);
-        stop.add(0,Stop);
 
-        SpatialSignal<Boolean> restaurant = new SpatialSignal<>(size);
-        ArrayList<Boolean> Restaurant=new ArrayList<Boolean>();
-        Restaurant.add(Boolean.FALSE);
-        Restaurant.add(Boolean.TRUE);
-        Restaurant.add(Boolean.FALSE);
-        Restaurant.add(Boolean.FALSE);
-        restaurant.add(0,Restaurant);
+        ArrayList<String> places = new ArrayList<>(Arrays.asList("BusStop", "Hospital", "MetroStop", "MainSquare", "BusStop", "Museum", "MetroStop"));
 
-        SpatialSignal<Boolean> hospital = new SpatialSignal<>(size);
-        ArrayList<Boolean> Hospital=new ArrayList<Boolean>();
-        Hospital.add(Boolean.FALSE);
-        Hospital.add(Boolean.FALSE);
-        Hospital.add(Boolean.TRUE);
-        Hospital.add(Boolean.FALSE);
-        hospital.add(0,Hospital);
 
+        SpatialSignal<Boolean> Taxi = new SpatialSignal<>(size);
+        ArrayList<Boolean> taxi=new ArrayList<>(Arrays.asList(false,false,true,false,false,true,false));
+        Taxi.add(0,taxi);
 
 
         double range = 10;
         DistanceStructure<Double, Double> minutes = new DistanceStructure<>(x -> x, new DoubleDistance(), d -> d<range, city);
 
-        ArrayList<Boolean> result = minutes.somewhere(new BooleanDomain(), i -> i<2 );
-        System.out.print(result);
 
+        //// notHospital property
+        ArrayList<Boolean> notHospital = new ArrayList<Boolean>();
+        places.forEach(i -> notHospital.add(!i.equals("Hospital")));
 
-        //ArrayList<Boolean> result = minutes.somewhere(new BooleanDomain(), i -> hospital.getSignals().get(i).getIterator(Boolean.TRUE);
+        //// Somewere Taxi property
+        ArrayList<Boolean> somewhereTaxy = minutes.somewhere(new BooleanDomain(), taxi::get );
+
+        //// Hospital -> Somewere Taxi property
+        ArrayList<Boolean> r1 = new ArrayList<Boolean>(size);
+        for(int i=0; i<size; i++) {
+            r1.add(notHospital.get(i)||somewhereTaxy.get(i));
+        }
+
+        System.out.print(r1);
+
     }
 
 
