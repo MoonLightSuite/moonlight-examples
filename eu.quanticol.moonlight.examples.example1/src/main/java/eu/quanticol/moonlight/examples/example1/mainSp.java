458f7b37b70c4f674c2b031318c2d8c93712965b
Simone <simone.silvetti@gmail.com>
2019-03-12 19:04:42 +0100

    expcity 2

--- eu.quanticol.moonlight.examples.example1/src/main/java/eu/quanticol/moonlight/examples/example1/mainSp.java
+++ eu.quanticol.moonlight.examples.example1/src/main/java/eu/quanticol/moonlight/examples/example1/mainSp.java
@@ -2,21 +2,11 @@ package eu.quanticol.moonlight.examples.example1;
 
 import eu.quanticol.moonlight.formula.BooleanDomain;
 import eu.quanticol.moonlight.formula.DoubleDistance;
-import eu.quanticol.moonlight.formula.SignalDomain;
-import eu.quanticol.moonlight.signal.DistanceStructure;
-import eu.quanticol.moonlight.signal.GraphModel;
-import eu.quanticol.moonlight.signal.SpatialModel;
-import eu.quanticol.moonlight.signal.SpatialSignal;
-import sun.font.TrueTypeFont;
-import java.util.*;
+import eu.quanticol.moonlight.signal.*;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.function.BiFunction;
-import java.util.stream.Collectors;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class mainSp {
@@ -44,15 +34,15 @@ public class mainSp {
         ArrayList<String> places = new ArrayList<>(Arrays.asList("BusStop", "Hospital", "MetroStop", "MainSquare", "BusStop", "Museum", "MetroStop"));
 
 
-        ArrayList<Boolean> taxi=new ArrayList<>(Arrays.asList(false,false,true,false,false,true,false));
-        ArrayList<Integer> people=new ArrayList<>(Arrays.asList(3,145,67,243,22,103,6));
+        ArrayList<Boolean> taxi = new ArrayList<>(Arrays.asList(false, false, true, false, false, true, false));
+        ArrayList<Integer> people = new ArrayList<>(Arrays.asList(3, 145, 67, 243, 22, 103, 6));
 
         SpatialSignal<Boolean> spCity = new SpatialSignal<>(size);
-        spCity.add(0,taxi);
+        spCity.add(0, taxi);
 
 
         double range = 10;
-        DistanceStructure<Double, Double> minutes = new DistanceStructure<>(x -> x, new DoubleDistance(), d -> d<range, city);
+        DistanceStructure<Double, Double> minutes = new DistanceStructure<>(x -> x, new DoubleDistance(), d -> d < range, city);
 
 
         //// notHospital property
@@ -60,20 +50,38 @@ public class mainSp {
         places.forEach(i -> notHospital.add(!i.equals("Hospital")));
 
         //// Somewere Taxi property
-        ArrayList<Boolean> somewhereTaxy = minutes.somewhere(new BooleanDomain(), taxi::get );
+        ArrayList<Boolean> somewhereTaxy = minutes.somewhere(new BooleanDomain(), taxi::get);
 
         //// Hospital -> Somewere Taxi property
         ArrayList<Boolean> r1 = new ArrayList<Boolean>(size);
-        for(int i=0; i<size; i++) {
-            r1.add(notHospital.get(i)||somewhereTaxy.get(i));
+        for (int i = 0; i < size; i++) {
+            r1.add(notHospital.get(i) || somewhereTaxy.get(i));
         }
 
         System.out.print(r1);
 
+        AssignmentFactory factory = new AssignmentFactory(String.class, Boolean.class, Integer.class);
+        factory.get("pippo", true, 5);
+        //variableArraySignal.add(0,);
+        List<VariableArraySignal> pippo = new ArrayList<>();
+        for (int j = 0; j < size; j++) {
+            VariableArraySignal variableArraySignal = new VariableArraySignal(new String[]{"place", "taxi", "people"}, factory);
+            variableArraySignal.add(0, places.get(j), taxi.get(j), people.get(j));
+            pippo.add(variableArraySignal);
+        }
+        ArrayList<Object[]> pluto = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            pluto.add(new Object[]{places.get(i), taxi.get(i), people.get(i)});
+        }
+        SpatialSignal<Object[]> sss = new SpatialSignal<>(size);
+        sss.add(0, pluto);
+
+        System.out.println();
+
     }
 
 
-    }
+}
 
 
 
