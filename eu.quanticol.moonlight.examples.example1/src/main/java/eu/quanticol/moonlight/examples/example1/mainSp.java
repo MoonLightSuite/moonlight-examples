1a40909d3c0b1cefa4fdebcd95cd09d0ec8e9195
lauranenzi <laura.nenzi@gmail.com>
2019-03-13 14:45:20 +0100

    prop

--- eu.quanticol.moonlight.examples.example1/src/main/java/eu/quanticol/moonlight/examples/example1/mainSp.java
+++ eu.quanticol.moonlight.examples.example1/src/main/java/eu/quanticol/moonlight/examples/example1/mainSp.java
@@ -4,9 +4,8 @@ import eu.quanticol.moonlight.formula.BooleanDomain;
 import eu.quanticol.moonlight.formula.DoubleDistance;
 import eu.quanticol.moonlight.signal.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
 
 
 public class mainSp {
@@ -35,46 +34,68 @@ public class mainSp {
         ArrayList<Integer> people=new ArrayList<>(Arrays.asList(3,145,67,243,22,103,6));
 
 
-        SpatialSignal<Boolean> spCity = new SpatialSignal<>(size);
-        spCity.add(0, taxi);
+        //// Stop property
+        ArrayList<Boolean> stop = new ArrayList<Boolean>();
+        place.forEach(i -> stop.add(i.equals("BusStop") || i.equals("MetroStop")));
+        System.out.println(stop);
 
-
-        double range = 10;
-        DistanceStructure<Double, Double> minutes = new DistanceStructure<>(x -> x, new DoubleDistance(), d -> d < range, city);
-
+        //// MainSquare property
+        ArrayList<Boolean> mainsquare = new ArrayList<Boolean>();
+        place.forEach(i -> mainsquare.add(i.equals("MainSquare")));
 
         //// notHospital property
         ArrayList<Boolean> notHospital = new ArrayList<Boolean>();
         place.forEach(i -> notHospital.add(!i.equals("Hospital")));
 
         //// Somewere Taxi property
+        double range = 10;
+        DistanceStructure<Double, Double> minutes = new DistanceStructure<>(x -> x, new DoubleDistance(), d -> d < range, city);
         ArrayList<Boolean> somewhereTaxy = minutes.somewhere(new BooleanDomain(), taxi::get);
 
-        //// Hospital -> Somewere Taxi property
+        //// (R1) Hospital -> Somewere Taxi property
         ArrayList<Boolean> r1 = new ArrayList<Boolean>(size);
         for (int i = 0; i < size; i++) {
             r1.add(notHospital.get(i) || somewhereTaxy.get(i));
         }
+        System.out.println(r1);
 
-        System.out.print(r1);
+        /// stop reach_{<=10} mainsquare
+        ArrayList<Boolean> reacmainsquare = minutes.reach(new BooleanDomain(), taxi::get, mainsquare::get);
+
+
+        System.out.println(reacmainsquare);
+
+
+
+
+
+
+
+
+        //// SpatioTemporalMonitoring
 
         AssignmentFactory factory = new AssignmentFactory(String.class, Boolean.class, Integer.class);
-        factory.get("pippo", true, 5);
-        //variableArraySignal.add(0,);
-        List<VariableArraySignal> pippo = new ArrayList<>();
-        for (int j = 0; j < size; j++) {
-            VariableArraySignal variableArraySignal = new VariableArraySignal(new String[]{"place", "taxi", "people"}, factory);
-            variableArraySignal.add(0, place.get(j), taxi.get(j), people.get(j));
-            pippo.add(variableArraySignal);
-        }
-        ArrayList<Object[]> pluto = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
-            pluto.add(new Object[]{place.get(i), taxi.get(i), people.get(i)});
-        }
-        SpatialSignal<Object[]> sss = new SpatialSignal<>(size);
-        sss.add(0, pluto);
+        HashMap<String,Integer> vTable = new HashMap<>();
+        vTable.put("place", 1);
+        vTable.put("taxi", 2);
+        vTable.put("people", 3);
 
-        System.out.println();
+        //VariableArraySignal variableArraySignal = new VariableArraySignal(new String[]{"place", "taxi", "people"}, factory);
+        // variableArraySignal.add(0, place.get(j), taxi.get(j), people.get(j));
+
+
+        ArrayList<Assignment> signalSP = new ArrayList<Assignment>();
+        for (int i = 0; i < size; i++) {
+            signalSP.add(factory.get(place.get(i), taxi.get(i), people.get(i)));
+        }
+
+        SpatioTemporalSignal<Assignment> citySignal = new SpatioTemporalSignal<>(size);
+        citySignal.add(0,signalSP);
+
+
+
+
+
 
     }
 
